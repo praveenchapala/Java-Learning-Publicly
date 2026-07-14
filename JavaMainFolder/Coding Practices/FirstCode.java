9a8661d7369a3c47b9d902b75a2626bda90be934
chapalapraveen <praveenpraveensimhadri@gmail.com>
2026-07-14 19:41:29 +0530

    Understood the concept of println and print

---
+++ JavaMainFolder/Coding Practices/FirstCode.java
@@ -0,0 +1,8 @@
+public class FirstCode{
+    public static void main(String args[]){
+        System.out.print("I want to develop applications -so iam learning java");
+        System.out.println("Full Stack development");
+        System.out.print("For developing Backend of an application");
+        System.out.println("Iam Learning java");
+    }
+}

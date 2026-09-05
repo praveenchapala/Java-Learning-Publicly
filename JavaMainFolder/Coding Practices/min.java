54ec3f06f89b18c73b731b4acae72ba5437f52f4
chapalapraveen <praveenpraveensimhadri@gmail.com>
2026-09-05 23:33:42 +0530

    min of array elements

--- JavaMainFolder/Coding Practices/min.java
+++ JavaMainFolder/Coding Practices/min.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class max{
+public class min{
     public static void main(String args[]){
         Scanner scanner = new Scanner(System.in);
         int arr[]= new int[5];
@@ -11,15 +11,14 @@ public class max{
         for(int i=0;i<arr.length-1;i++){
             System.out.println(arr[i]);
         }
-        int max=arr[0];
+        int min=arr[0];
         for(int i=0;i<arr.length-1;i++){
-            if(arr[i]>max){
-                max=arr[i];
+            if(arr[i]<min){
+                min=arr[i];
             }
             
-            
         }
-        System.out.println("the max of the elements of an array is:"+max);
+        System.out.println("the min of the elements of an array is:"+min);
     
     }
 }

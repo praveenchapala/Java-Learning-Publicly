19aea9c5a3d1a95f7acf045488603d1d2cfc1078
chapalapraveen <praveenpraveensimhadri@gmail.com>
2026-09-07 22:37:01 +0530

    practiced question

---
+++ JavaMainFolder/Coding Practices/arr.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class arr {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int size =scanner.nextInt();
+        int arr[] = new int[size];
+        for(int i=0;i<size;i++){
+            arr[i] =scanner.nextInt();
+        }
+
+        for(int i=0;i<size;i++){
+            System.out.println(arr[i]);
+        }
+
+    }
+    
+}

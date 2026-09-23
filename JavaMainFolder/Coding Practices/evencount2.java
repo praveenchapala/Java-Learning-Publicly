036cde582b37fd9256b860dd5f2a9ec2a8449e46
chapalapraveen <praveenpraveensimhadri@gmail.com>
2026-09-23 18:46:18 +0530

    evencount2

---
+++ JavaMainFolder/Coding Practices/evencount2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class evencount2{
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int arr[] = new int[size];
+        for(int i=0;i<size;i++){
+            arr[i] = scanner.nextInt();
+        }
+        int evencount = 0;
+        for(int i=0;i<size;i++){
+            if(arr[i]%2==0){
+                evencount++;
+            }
+        }
+        System.out.println("Even count: "+evencount);
+    }
+}
